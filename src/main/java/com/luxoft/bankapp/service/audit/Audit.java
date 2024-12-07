bb885b24186cb1104bd20dfb3284ba415d84e88d
dariusbrinzan <brinzandarius27@gmail.com>
2024-12-07 11:56:50 +0200

    finished task 3 & 4

--- src/main/java/com/luxoft/bankapp/service/audit/Audit.java
+++ src/main/java/com/luxoft/bankapp/service/audit/Audit.java
@@ -1,6 +1,13 @@
 package com.luxoft.bankapp.service.audit;
+
+import com.luxoft.bankapp.service.audit.events.BalanceEvent;
+import com.luxoft.bankapp.service.audit.events.DepositEvent;
+import com.luxoft.bankapp.service.audit.events.WithdrawEvent;
+
 public interface Audit {
-    void auditDeposit(long accountId, double amount);
-    void auditWithdraw(long accountId, double amount, WithdrawState state);
-    void auditBalance(long accountId);
+    void auditOperation(DepositEvent event);
+
+    void auditOperation(WithdrawEvent event);
+
+    void auditOperation(BalanceEvent event);
 }
--- src/main/java/com/luxoft/bankapp/service/audit/AuditAspect.java
+++ src/main/java/com/luxoft/bankapp/service/audit/AuditAspect.java
@@ -1,6 +1,10 @@
 package com.luxoft.bankapp.service.audit;
+
 import com.luxoft.bankapp.model.Account;
 import com.luxoft.bankapp.model.Client;
+import com.luxoft.bankapp.service.audit.events.BalanceEvent;
+import com.luxoft.bankapp.service.audit.events.DepositEvent;
+import com.luxoft.bankapp.service.audit.events.WithdrawEvent;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -8,58 +12,85 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
+
 @Configuration
 @Aspect
 public class AuditAspect {
     @Autowired
-    private Audit audit;
+    private ApplicationContext applicationContext;
+
+
     @Before("anyDeposit()")
     public void logDeposit(JoinPoint joinPoint)
     {
         Object[] methodArgs = joinPoint.getArgs();
-        audit.auditDeposit(getAccountId(methodArgs),
-                (double) methodArgs[1]);
+
+        applicationContext.publishEvent(
+                new DepositEvent(getAccountId(methodArgs),
+                        (double) methodArgs[1]));
     }
+
     @Before("anyBalance()")
     public void logBalance(JoinPoint joinPoint)
     {
         Object[] methodArgs = joinPoint.getArgs();
-        audit.auditBalance(getAccountId(methodArgs));
+
+        applicationContext.publishEvent(
+                new BalanceEvent(getAccountId(methodArgs)));
     }
+
     @Around("anyWithdraw()")
     public Object logWithdrawal(ProceedingJoinPoint thisJoinPoint) throws Throwable
     {
         Object[] methodArgs = thisJoinPoint.getArgs();
+
         long accountId = getAccountId(methodArgs);
-        audit.auditWithdraw(accountId,
-                (double) methodArgs[1], WithdrawState.TRYING);
+
+        applicationContext.publishEvent(
+                new WithdrawEvent(getAccountId(methodArgs),
+                        (double) methodArgs[1]));
+
         Object result;
+
         try
         {
             result = thisJoinPoint.proceed();
-            audit.auditWithdraw(accountId,
-                    (double) methodArgs[1],
-                    WithdrawState.SUCCESSFUL);
+
+            applicationContext.publishEvent(
+                    new WithdrawEvent(getAccountId(methodArgs),
+                            (double) methodArgs[1],
+                            WithdrawEvent.State.SUCCESSFUL));
+
         }
         catch (Exception e)
         {
-            audit.auditWithdraw(accountId,
-                    (double) methodArgs[1],
-                    WithdrawState.FAILED);
+            applicationContext.publishEvent(
+                    new WithdrawEvent(getAccountId(methodArgs),
+                            (double) methodArgs[1],
+                            WithdrawEvent.State.FAILED));
             throw e;
         }
+
         return result;
     }
+
+
+
     @Pointcut("execution(* com.luxoft.bankapp.service.operations.*.deposit(..))")
     public void anyDeposit() {}
+
     @Pointcut("execution(* com.luxoft.bankapp.service.operations.*.withdraw(..))")
     public void anyWithdraw() {}
+
     @Pointcut("execution(* com.luxoft.bankapp.service.operations.*.getBalance(..))")
     public void anyBalance() {}
+
     private long getAccountId(Object[] methodArgs)
     {
         Account account;
+
         if (methodArgs[0] instanceof Client)
         {
             account = ((Client)  methodArgs[0])
@@ -69,6 +100,8 @@ public class AuditAspect {
         {
             account = (Account) methodArgs[0];
         }
+
         return account.getId();
     }
+
 }
